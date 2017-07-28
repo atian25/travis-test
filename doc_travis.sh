5fc76f13eb7c8189ddceae3b77d76f6041538b58
TZ <atian25@qq.com>
2017-07-28 11:57:24 +0800

    test: deploy on at 8

--- doc_travis.sh
+++ doc_travis.sh
@@ -4,12 +4,13 @@
 
 # Only deply on master branch
 SOURCE_BRANCH="master"
+BUILD_VERSION="8"
 
 echo "Node version: $TRAVIS_NODE_VERSION"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-  echo "Skip deploy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST, TRAVIS_BRANCH=$TRAVIS_BRANCH"
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH"  -o "$TRAVIS_NODE_VERSION" != "$BUILD_VERSION" ]; then
+  echo "Skip deploy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST, TRAVIS_BRANCH=$TRAVIS_BRANCH, NODE_VERSION=$TRAVIS_NODE_VERSION"
   exit 0
 fi
 
